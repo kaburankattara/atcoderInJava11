0459ee091c81bf398c253ae93ee48f5d2ed0f13f
tmpuser-at-498588106976 <>
2022-11-27 15:52:49 +0900

    RANDOM　解答　全テストクリア

--- src/main/java/com/atcoder/in/java11/RANDOM/Main.java
+++ src/main/java/com/atcoder/in/java11/RANDOM/Main.java
@@ -2,9 +2,7 @@ package com.atcoder.in.java11.RANDOM;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
@@ -12,59 +10,64 @@ public class Main {
         Scanner scan = new Scanner(new File("src/main/java/com/atcoder/in/java11/RANDOM/input4.txt"));
 
         String[] firstStr = scan.nextLine().split(" ");
-        final int h = toInt(firstStr[0]);
-        final int w = toInt(firstStr[1]);
+        int h = toInt(firstStr[0]);
+        int w = toInt(firstStr[1]);
 
         // 図Sをリストで表現
-        List<List<Character>> sWidths = new ArrayList<>();
+        List<StringBuilder> sWidths = new ArrayList<>();
         for (int i = 0; i < w; i++) {
-            sWidths.add(new ArrayList<>());
+            sWidths.add(new StringBuilder());
         }
         for (int i = 0; i < h; i++) {
             String wValue = scan.nextLine();
             int j = 0;
             for (char ch : wValue.toCharArray()) {
-                sWidths.get(j).add(ch);
+                sWidths.get(j).append(ch);
                 j++;
             }
         }
+        Map<String, Integer> sWidthsRes = new HashMap<>();
+        for (StringBuilder sb : sWidths) {
+            String key = sb.toString();
+            sWidthsRes.put(key,sWidthsRes.getOrDefault(key,0) + 1);
+        }
+
 
         // 図Tをリストで表現
-        List<List<Character>> tWidths = new ArrayList<>();
+        List<StringBuilder> tWidths = new ArrayList<>();
         for (int i = 0; i < w; i++) {
-            tWidths.add(new ArrayList<>());
+            tWidths.add(new StringBuilder());
         }
         for (int i = 0; i < h; i++) {
             String wValue = scan.nextLine();
             int j = 0;
             for (char ch : wValue.toCharArray()) {
-                tWidths.get(j).add(ch);
+                tWidths.get(j).append(ch);
                 j++;
             }
         }
+        Map<String, Integer> tWidthsRes = new HashMap<>();
+        for (StringBuilder sb : tWidths) {
+            String key = sb.toString();
+            tWidthsRes.put(key,tWidthsRes.getOrDefault(key,0) + 1);
+        }
 
-        for (List<Character> sStr : sWidths) {
-            boolean isMatch = false;
-            int currentI = 0;
-            for (List<Character> tStr : tWidths) {
-                // 列が一致しなければ、スキップ
-                if (!isEquals(sStr, tStr, h)) {
-                    currentI++;
-                    continue;
-                }
-
-                isMatch = true;
-                break;
-            }
-
-            // 図Sの列にマッチする列が図Tになければ、結果はNo
-            if (!isMatch) {
+        for (Map.Entry sStr : sWidthsRes.entrySet()) {
+            String key = (String) sStr.getKey();
+            int value = (int) sStr.getValue();
+            // 列が一致しなければ、スキップ
+            if (!tWidthsRes.containsKey(key)) {
                 System.out.println("No");
                 return;
             }
 
-            tWidths.remove(currentI);
-
+            tWidthsRes.put(key, tWidthsRes.get(key) - value);
+            if (tWidthsRes.get(key).compareTo(0) == 0) {
+                tWidthsRes.remove(key);
+            } else if (tWidthsRes.get(key).compareTo(0) < 0) {
+                System.out.println("No");
+                return;
+            }
         }
 
         System.out.println("Yes");
@@ -74,12 +77,4 @@ public class Main {
         return Integer.parseInt(a);
     }
 
-    public static boolean isEquals(final List<Character> list1, final List<Character> list2, final int length) {
-        for (int i = 0; i < length; i++) {
-            if (!(list1.get(i).charValue() == list2.get(i).charValue())) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
