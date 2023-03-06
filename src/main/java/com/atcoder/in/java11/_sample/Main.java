4663754a4d939d27021c667f34bb129ec061ef38
kaburankattara <kaburankattara@gmail.com>
2023-03-06 21:53:37 +0900

    sampleファイル類を追加

---
+++ src/main/java/com/atcoder/in/java11/_sample/Main.java
@@ -0,0 +1,19 @@
+package com.atcoder.in.java11._sample;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.*;
+
+public class Main {
+
+    private static PrintStream out = System.out;
+
+    public static void main(String[] args) throws FileNotFoundException {
+//        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(new File("src/main/java/com/atcoder/in/java11/_sample/input1.txt"));
+
+        out.println("Yes");
+    }
+
+}
